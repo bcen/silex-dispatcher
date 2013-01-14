5619db59adca3591d47bd93fe9d57520a9cf1ca6
bcen <bocai.cen@gmail.com>
2013-01-13 22:54:07 -0500

    Added alias for all dispatching service.
    * Moved "sdispatcher.template_renderer" to be availiable only after boot.

--- src/SDispatcher/DispatchingServiceProvider.php
+++ src/SDispatcher/DispatchingServiceProvider.php
@@ -30,19 +30,32 @@ class DispatchingServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
-        $app[DispatchingServiceProvider::CLASS_RESOLVER] = $app->share(function($container) {
-            return new ClassResolver($container, array('silex' => $container));
-        });
+        $app[DispatchingServiceProvider::CLASS_RESOLVER] = $app->share(
+            function($container) {
+                return new ClassResolver(
+                    $container,
+                    array('Silex\\Application' => $container)
+                );
+            }
+        );
 
-        $app[DispatchingServiceProvider::CONTROLLER_FACTORY] = $app->share(function($container) {
-            return new ControllerFactory($container[DispatchingServiceProvider::CLASS_RESOLVER]);
-        });
+        // alias of DispatchingServiceProvider::CLASS_RESOLVER
+        $app['SDispatcher\Common\ClassResolver'] = function($c) {
+            return $c[DispatchingServiceProvider::CLASS_RESOLVER];
+        };
 
-        if (isset($app['twig']) && $app['twig'] instanceof Twig_Environment) {
-            $app[DispatchingServiceProvider::TEMPLATE_RENDERER] = $app->share(function($container) {
-                return new TwigRendererAdapter($container['twig']);
-            });
-        }
+        $app[DispatchingServiceProvider::CONTROLLER_FACTORY] = $app->share(
+            function($container) {
+                return new ControllerFactory(
+                    $container[DispatchingServiceProvider::CLASS_RESOLVER]
+                );
+            }
+        );
+
+        // alias of DispatchingServiceProvider::CONTROLLER_FACTORY
+        $app['SDispatcher\\ControllerFactory'] = function($c) {
+            return $c[DispatchingServiceProvider::CONTROLLER_FACTORY];
+        };
     }
 
     /**
@@ -50,5 +63,17 @@ class DispatchingServiceProvider implements ServiceProviderInterface
      */
     public function boot(Application $app)
     {
+        if (isset($app['twig']) && $app['twig'] instanceof Twig_Environment) {
+            $app[DispatchingServiceProvider::TEMPLATE_RENDERER] = $app->share(
+                function($container) {
+                    return new TwigRendererAdapter($container['twig']);
+                }
+            );
+
+            $app['SDispatcher\\TemplateEngine\\TwigRendererAdapter'] =
+                function($c) {
+                    return $c[DispatchingServiceProvider::TEMPLATE_RENDERER];
+                };
+        }
     }
 }
--- tests/SDispatcher/Tests/DispatchingServiceProviderTest.php
+++ tests/SDispatcher/Tests/DispatchingServiceProviderTest.php
@@ -12,10 +12,12 @@ class DispatchingServiceProviderTest extends \PHPUnit_Framework_TestCase
     public function should_register_ClassResolver_and_ControllerFactory()
     {
         $app = new Application();
-        $app->register(new \Silex\Provider\TwigServiceProvider());
         $app->register(new DispatchingServiceProvider());
         $this->assertInstanceOf('SDispatcher\\Common\\ClassResolver', $app[DispatchingServiceProvider::CLASS_RESOLVER]);
         $this->assertInstanceOf('SDispatcher\\ControllerFactory', $app[DispatchingServiceProvider::CONTROLLER_FACTORY]);
+        $app->register(new \Silex\Provider\TwigServiceProvider());
+
+        $app->boot();
         $this->assertInstanceof('SDispatcher\\TemplateEngine\\TwigRendererAdapter', $app[DispatchingServiceProvider::TEMPLATE_RENDERER]);
     }
 }
