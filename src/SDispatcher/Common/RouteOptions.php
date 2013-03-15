4c1ca3414d5bcabe0c0a014e8d2337229d23c253
bcen <bocai.cen@gmail.com>
2013-03-15 12:20:47 -0400

    I love alignment.

--- src/SDispatcher/Common/RouteOptions.php
+++ src/SDispatcher/Common/RouteOptions.php
@@ -3,12 +3,12 @@ namespace SDispatcher\Common;
 
 class RouteOptions
 {
-    const SUPPORTED_FORMATS = 'sdispatcher.route.supported_formats';
-    const RESOURCE_ID = 'sdispatcher.route.resource_identifier';
-    const PAGE_LIMIT = 'sdispatcher.route.page_limit';
-    const WILL_PAGINGATE = 'sdispatcher.route.will_paginate';
-    const ACCEPTED_FORMAT = 'sdispatcher.route.accepted_format';
-    const PAGINATOR_CLASS = 'sdispatcher.route.paginator_class';
+    const SUPPORTED_FORMATS             = 'sdispatcher.route.supported_formats';
+    const RESOURCE_ID                   = 'sdispatcher.route.resource_identifier';
+    const PAGE_LIMIT                    = 'sdispatcher.route.page_limit';
+    const WILL_PAGINGATE                = 'sdispatcher.route.will_paginate';
+    const ACCEPTED_FORMAT               = 'sdispatcher.route.accepted_format';
+    const PAGINATOR_CLASS               = 'sdispatcher.route.paginator_class';
     const PAGINATED_DATA_CONTAINER_NAME = 'sdispatcher.route.paginated_data_container_name';
     const PAGINATED_META_CONTAINER_NAME = 'sdispatcher.route.paginated_meta_container_name';
 }
