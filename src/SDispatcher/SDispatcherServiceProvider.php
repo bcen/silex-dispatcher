369eed2b9d996d2eb68c3a280f70b2f42b0bcb92
bcen <bocai.cen@gmail.com>
2013-03-24 09:33:18 -0400

    Added more doc comments.

--- src/SDispatcher/SDispatcherServiceProvider.php
+++ src/SDispatcher/SDispatcherServiceProvider.php
@@ -10,6 +10,19 @@ use SDispatcher\Middleware\SerializationInspector;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
+/**
+ * It registers:
+ * - `resolver`
+ * - `sdispatcher.middleware.global`
+ * <p/>
+ * If `sdispatcher.middleware.global` is true, it will register the middlewares
+ * into the global scope; otherwise, consumer must register the middleware
+ * separately.
+ * </p>
+ * Also, there are two event subscriber will be registered:
+ * - `SDispatcher\Middleware\PaginationListener`
+ * - `SDispatcher\Middleware\ArrayToDataResponseListener`
+ */
 class SDispatcherServiceProvider implements ServiceProviderInterface
 {
     /**
@@ -17,9 +30,11 @@ class SDispatcherServiceProvider implements ServiceProviderInterface
      */
     public function register(Application $app)
     {
-        $app['resolver'] = $app->share($app->extend('resolver', function ($resolver, $app) {
-            return new ControllerResolver($resolver, $app);
-        }));
+        $app['resolver'] = $app->share($app->extend(
+                'resolver',
+                function ($resolver, $app) {
+                    return new ControllerResolver($resolver, $app);
+                }));
 
         $globalMiddlewareId = 'sdispatcher.middleware.global';
 
@@ -34,10 +49,10 @@ class SDispatcherServiceProvider implements ServiceProviderInterface
             $app->after(new SerializationInspector($app['routes']));
         }
 
-        /* @var \Symfony\Component\EventDispatcher\EventDispatcher $dispatcher */
-        $dispatcher = $app['dispatcher'];
-        $dispatcher->addSubscriber(new PaginationListener($app['routes']));
-        $dispatcher->addSubscriber(new ArrayToDataResponseListener($app['routes']));
+        /* @var \Symfony\Component\EventDispatcher\EventDispatcher $ed */
+        $ed = $app['dispatcher'];
+        $ed->addSubscriber(new PaginationListener($app['routes']));
+        $ed->addSubscriber(new ArrayToDataResponseListener($app['routes']));
     }
 
     /**
