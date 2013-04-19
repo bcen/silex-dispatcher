74c301bfbbfc81327c76931f91cd213af9677b3d
bcen <bocai.cen@gmail.com>
2013-04-18 21:56:52 -0400

    Added a default XML encoder instead of Symfony/Serializer's encoder

---
+++ spec/SDispatcher/Common/DefaultXmlEncoder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace spec\SDispatcher\Common;
+
+use PHPSpec2\ObjectBehavior;
+
+class DefaultXmlEncoder extends ObjectBehavior
+{
+    public function it_should_be_initializable()
+    {
+        $this->shouldHaveType('SDispatcher\Common\DefaultXmlEncoder');
+    }
+
+    public function its_supportsEncoding_should_return_true_if_format_is_xml()
+    {
+        $this->supportsEncoding('xml')->shouldReturn(true);
+    }
+
+    public function its_encode_should_return_correct_format_in_xml_string()
+    {
+        $data = array(
+            'message' => 'Hello World'
+        );
+        $expected = '<?xml version="1.0" encoding="utf-8"?>' . "\n" .
+                    '<response><message>Hello World</message></response>' . "\n";
+        $this->encode($data, 'xml')->shouldReturn($expected);
+    }
+}
