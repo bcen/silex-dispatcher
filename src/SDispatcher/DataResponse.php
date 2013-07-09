7dbddb04c731f3d8f57822c292c50a87aa2d6615
bcen <bocai.cen@gmail.com>
2013-07-09 14:37:59 -0400

    Added bunch of convenient method for DataResponse

--- src/SDispatcher/DataResponse.php
+++ src/SDispatcher/DataResponse.php
@@ -20,4 +20,59 @@ class DataResponse extends Response
     {
         return $this->content;
     }
+
+    public static function ok($content, $headers = array())
+    {
+        return new static($content, 200, $headers);
+    }
+
+    public static function created($content, $headers = array())
+    {
+        return new static($content, 201, $headers);
+    }
+
+    public static function accepted($content, $headers = array())
+    {
+        return new static($content, 202, $headers);
+    }
+
+    public static function noContent($headers = array())
+    {
+        return new static('', 204, $headers);
+    }
+
+    public static function badRequest($content, $headers = array())
+    {
+        return new static($content, 400, $headers);
+    }
+
+    public static function unauthorized($content, $headers = array())
+    {
+        return new static($content, 401, $headers);
+    }
+
+    public static function forbidden($content, $headers = array())
+    {
+        return new static($content, 403, $headers);
+    }
+
+    public static function notFound($content, $headers = array())
+    {
+        return new static($content, 404, $headers);
+    }
+
+    public static function methodNotAllowed($content, $headers = array())
+    {
+        return new static($content, 405, $headers);
+    }
+
+    public static function notAcceptable($content, $headers = array())
+    {
+        return new static($content, 406, $headers);
+    }
+
+    public static function conflict($content, $headers = array())
+    {
+        return new static($content, 409, $headers);
+    }
 }
